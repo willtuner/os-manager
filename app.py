bc3cf7a5fd4bc12a61b338af4b4ed3298fdc83c4
Wilson Santana de Almeida <wilsonsantana@prats.net>
2025-04-22 15:02:27 -0300

    ajuste final: host 0.0.0.0 e porta dinâmica sem debug

--- app.py
+++ app.py
@@ -119,6 +119,7 @@ def ler_csv(caminho):
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
