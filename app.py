395a1b52a2ac3e38ae49db6732ed4b99a7bf8428
Wilson Santana de Almeida <wilsonsantana@prats.net>
2025-04-22 14:58:56 -0300

    corrigindo host para 0.0.0.0 para funcionar no Render

--- app.py
+++ app.py
@@ -116,5 +116,9 @@ def ler_csv(caminho):
     with open(caminho, encoding="utf-8") as f:
         return list(csv.DictReader(f))
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
