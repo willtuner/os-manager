fdf8724e9ff2cf117a233517cffc9a196b1dc992
willtuner <62344571+willtuner@users.noreply.github.com>
2025-04-26 10:09:47 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
+pip install Flask-SQLAlchemy Flask-Migrate
 pip install --upgrade pip
 pip install --force-reinstall numpy==1.24.3
 pip install -r requirements.txt
