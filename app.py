c68f7147bf9b79cf84b82c7eefad98ae66b97e0c
willtuner <62344571+willtuner@users.noreply.github.com>
2025-07-16 17:06:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1163,3 +1163,41 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0',
            port=int(os.environ.get('PORT', 10000)),
            debug=True)
+
+@app.route('/frota-leve')
+def frota_leve():
+    if not session.get('is_admin'):
+        return redirect('/login')
+
+    filtro = request.args.get('filtro')
+    query = FrotaLeve.query
+
+    if filtro and filtro.lower() != "todos":
+        query = query.filter(FrotaLeve.situacao.ilike(f'%{filtro}%'))
+
+    dados = query.all()
+
+    return render_template(
+        'frota_leve.html',
+        dados=dados,
+        usuario=session.get('gerente') or session.get('manutencao') or session.get('prestador')
+    )
+
+@app.route('/frota-leve')
+def frota_leve():
+    if not session.get('is_admin'):
+        return redirect('/login')
+
+    filtro = request.args.get('filtro')
+    query = FrotaLeve.query
+
+    if filtro and filtro.lower() != "todos":
+        query = query.filter(FrotaLeve.situacao.ilike(f'%{filtro}%'))
+
+    dados = query.all()
+
+    return render_template(
+        'frota_leve.html',
+        dados=dados,
+        usuario=session.get('gerente') or session.get('manutencao') or session.get('prestador')
+    )
